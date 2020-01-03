a83effa50811272301b5547dc6ad39b36b08bc36
Toxiad <676953318@qq.com>
2020-01-03 18:01:34 +0800

    tr

---
+++ BiLiRoku/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using BiliRoku.Bililivelib;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace BiliRoku
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        private RoomList _roomlist;
+
+        public MainWindow()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = (sender, cert, chain, errors) => true;
+            _roomlist = new RoomList();
+            InitializeComponent();
+        }
+
+        public void AppendLogln(string source, string level, string logText)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                infoBlock.AppendText($"[{source}] [{level} {DateTime.Now.ToString("HH:mm:ss")}] {logText}\n");
+            });
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _roomlist.RestoreRooms();
+            roomListView.ItemsSource = _roomlist;
+
+            var _config = Config.Instance;
+            //显示更新说明。
+            if (_config.Version != Ver.VER)
+            {
+                MessageBox.Show("BiliRoku已经更新到 " + Ver.VER + "\n\n更新说明：\n" + Ver.DESC);
+                _config.Version = Ver.VER;
+            }
+            AppendLogln("Core", "INFO", "启动成功。");
+            var checkUpdate = new CheckUpdate();
+            checkUpdate.OnResult += CheckUpdate_OnResult;
+            InfoLogger.OnInfo += InfoLogger_OnInfo;
+        }
+
+        private void InfoLogger_OnInfo(InfoArgs info)
+        {
+            AppendLogln(info.source, info.level, info.info);
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckUpdate_OnResult(object sender, UpdateResultArgs result)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                About.Content = "发现新版本：" + result.version;
+                About.Click -= About_Click;
+                About.Click += (s, e) =>
+                {
+                    System.Diagnostics.Process.Start("explorer.exe", result.url);
+                };
+            });
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            var about = new About { Owner = this };
+            about.ShowDialog();
+        }
+
+        private void infoBlock_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            infoBlock.ScrollToEnd();
+        }
+
+        private void AddRoomButton_Click(object sender, RoutedEventArgs e)
+        {
+            var addRoomDialog = new AddRoom { Owner = this };
+            if(addRoomDialog.ShowDialog() == true)
+            {
+                var roomidString = addRoomDialog.roomid.Text;
+                if(long.TryParse(roomidString, out long roomid))
+                {
+                    _roomlist.AddRoom(roomid.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("房间号必须为一个整数。", "输入错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+                
+            }
+        }
+
+        private void DeleteRoom_Click(object sender, RoutedEventArgs e)
+        {
+            var source = (RoomTask)((Button)e.Source).DataContext;
+            source.Destroy();
+        }
+
+        private void OpenSettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            var settingDialog = new SavePathSetting { Owner = this };
+            settingDialog.ShowDialog();
+        }
+
+        private void RefreshAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            _roomlist.RefreshInfo();
+        }
+
+        private void RoomTaskMainButton_Click(object sender, RoutedEventArgs e)
+        {
+            var source = (RoomTask)((Button)e.Source).DataContext;
+            source.StartButton();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            AppendLogln("Core", "INFO", "准备关闭，等待所有活动录像结束后退出。");
+            _roomlist.DestroyAll();
+        }
+
+        private void CLS_Click(object sender, RoutedEventArgs e)
+        {
+            infoBlock.Text = "";
+            AppendLogln("CLIENT", "INFO", "已清空日志面板");
+        }
+    }
+}
---
+++ BiLiRoku/SavePathSetting.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BiliRoku
+{
+    /// <summary>
+    /// SavePathSetting.xaml 的交互逻辑
+    /// </summary>
+    public partial class SavePathSetting
+    {
+        private Config _config;
+        private SaveFileDialog _sfd;
+
+        public SavePathSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _config = Config.Instance;
+            _sfd = new SaveFileDialog {FileName = "savefile.flv"};
+            if (_config.SavePath != null)
+            {
+                SaveDirBox.Text = _config.SavePath;
+                _sfd.InitialDirectory = _config.SavePath;
+            }
+            if (_config.Filename != null)
+            {
+                FilenameBox.Text = _config.Filename;
+            }
+            if (_config.RefreshTime != null)
+            {
+                refreshTimeBox.Text = _config.RefreshTime;
+            }
+            if (_config.Timeout != null)
+            {
+                timeoutBox.Text = _config.Timeout;
+            }
+            SaveCommetCheckBox.IsChecked = _config.IsDownloadComment;
+            AutoRecordCheckBox.IsChecked = _config.IsWaitStreaming;
+            AutoRetryCheckBox.IsChecked = _config.IsAutoRetry;
+        }
+
+        private void OpenSaveDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(_sfd.ShowDialog() == true)
+            {
+                SaveDirBox.Text = System.IO.Path.GetDirectoryName(_sfd.FileName);
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SaveDirBox.Text == "")
+            {
+                MessageBox.Show("保存路径不能为空。", "BiliRoku");
+                return;
+            }
+            if (!System.IO.Directory.Exists(SaveDirBox.Text))
+            {
+                if (MessageBoxResult.OK != MessageBox.Show("目录不存在，确认将创建此目录", "确认？", MessageBoxButton.OKCancel))
+                {
+                    return;
+                }
+            }
+            if (FilenameBox.Text == "")
+            {
+                MessageBox.Show("文件名不能为空。", "BiliRoku");
+                return;
+            }
+            if (!System.IO.Path.HasExtension(FilenameBox.Text))
+            {
+                if (MessageBoxResult.OK == MessageBox.Show("文件路径不含扩展名。确认将自动添加“.flv”的扩展名。", "确认？", MessageBoxButton.OKCancel))
+                {
+                    FilenameBox.Text += ".flv";
+                }
+                else
+                {
+                    return;
+                }
+            }
+            if (!int.TryParse(refreshTimeBox.Text, out _))
+            {
+                MessageBox.Show("刷新间隔必须为整数。", "BiliRoku");
+                return;
+            }
+            if (!int.TryParse(timeoutBox.Text, out _))
+            {
+                MessageBox.Show("超时时间必须为整数。", "BiliRoku");
+                return;
+            }
+            if (_config != null)
+            {
+                _config.SavePath = SaveDirBox.Text;
+                _config.Filename = FilenameBox.Text;
+                _config.RefreshTime = refreshTimeBox.Text;
+                _config.Timeout = timeoutBox.Text;
+                _config.IsDownloadComment = SaveCommetCheckBox.IsChecked ?? false;
+                _config.IsWaitStreaming = AutoRecordCheckBox.IsChecked ?? false;
+                _config.IsAutoRetry = AutoRetryCheckBox.IsChecked ?? false;
+            }
+            DialogResult = true;
+            Close();
+        }
+
+        private void SaveNameHelp_Click(object sender, RoutedEventArgs e)
+        {
+            var flnhlp = new FileNameHelp { Owner = this };
+            flnhlp.ShowDialog();
+        }
+    }
+}
